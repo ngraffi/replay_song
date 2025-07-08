5bd18c253f87535f9cc426f7295d8d17a3bb156f
ngraffi <43457485+ngraffi@users.noreply.github.com>
2025-07-08 10:23:03 +0900

    Update loop_play.py

--- loop_play.py
+++ loop_play.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-upload_file = st.file_uploader("Choose a file")
+st.set_page_config(
+    page_title = "WooChan~",
+    page_icon = "😀"
+)
+
+upload_file = st.file_uploader("MP3 파일을 선택해주세요", type = ["mp3"])
 
 if upload_file is not None:
-    st.audio(upload_file)
+    st.audio(upload_file, loop=True, autoplay=True)
