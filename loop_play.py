89b3be846332e94f2eccbf96a17ac9e9d6c8f0b4
ngraffi <43457485+ngraffi@users.noreply.github.com>
2025-07-08 14:49:52 +0900

    Update loop_play.py

--- loop_play.py
+++ loop_play.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title = "WooChan~",
+    page_title = "WooChan's hymn",
     page_icon = "😀"
 )
+
+st.subheader("WooChan's hymn")
 
 upload_file = st.file_uploader("MP3 파일을 선택해주세요", type = ["mp3"])
 
